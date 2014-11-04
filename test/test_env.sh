fb7742d90a16b1d177843fc9c5ade2ae2b16aec1
ramadini <amadini@cs.unibo.it>
2014-11-04 10:48:46 +0100

    Using mznc14 version

--- test/test_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-TIMEOUT=30
-PROB_DIR="$SUNNY_HOME/benchmarks"
-LISTFILE="$SUNNY_HOME/test/benchmarks.list"
-RESULTS_DIR="$SUNNY_HOME/test/results"
-UNSAT_LIST="$SUNNY_HOME/test/unsat.list"
-OPT_LIST="$SUNNY_HOME/test/opt.list"
