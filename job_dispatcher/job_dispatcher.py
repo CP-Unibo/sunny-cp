c1a5f5e4fc88554ccd759518fb9d6ae5fee77dd9
jacopo <jacopom@ifi.uio.no>
2018-02-19 12:19:04 +0100

    checker anomalies

--- job_dispatcher/job_dispatcher.py
+++ job_dispatcher/job_dispatcher.py
@@ -27,6 +27,7 @@ QUEUE = Queue.Queue()
 MZN_ID = 0
 DZN_ID = 1
 SOL_ID = 2
+OPTIONS_ID = 3
 
 SLEEP_TIME = 1
 
@@ -99,11 +100,15 @@ def get_mzn_dzn_pairs(dir_name):
               type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True,
                               resolve_path=True),
               default=[])
+@click.option('--extra-options',
+              help='Extra options for calling sunny, comma separated. E.g., -P=gecode,-a=',
+              default="")
 def create_request_list(
         request_file,
         problems_dir,
         solver,
-        database_file
+        database_file,
+        extra_options
         ):
     '''
     Create the request list using mzn, dzn files in a directory.
@@ -138,7 +143,7 @@ def create_request_list(
     else:
         with open(request_file,'wb') as f:
             for (x,y,z) in [(x,y,z) for z in solver for (x,y) in mzn_dzn_pair]:
-                f.write("{}|{}|{}\n".format(x,y,z))
+                f.write("{}|{}|{}|{}\n".format(x,y,z,extra_options))
 
 cli.add_command(create_request_list)
 
@@ -175,12 +180,13 @@ def parse_solver_output(output):
                     result["time"] = time
             elif line.startswith("=========="):
                 if optimization_problem:
-                    if len(result["solutions"]) > 0:
-                        result["result"] = "opt"
-                    else:
-                        result["result"] = "uns"
-                else:
-                    result["result"] = "uns"
+                    result["result"] = "opt"
+            elif line.startswith("=====UNSATISFIABLE====="):
+                result["result"] = "uns"
+            elif line.startswith("=====UNBOUNDED====="):
+                result["result"] = "unb"
+            elif line.startswith("=====UNKNOWN====="):
+                result["result"] = "unk"
             elif line.startswith("% Search completed at time: "):
                 result["time"] = float(line[len("% Search completed at time: "):])
         return result
@@ -262,6 +268,13 @@ def worker(thread_num,database_file,timeout,url,hostname):
                     files['dzn'] = open(item[DZN_ID], 'rb')
                 if goal != "sat":
                     files['-a'] = ""
+                if item[OPTIONS_ID]:
+                    ls = [ x.split("=") for x in OPTIONS_ID.split(",")]
+                    ls_errors = [ x for x in ls if len(x) != 2]
+                    if ls_errors:
+                        logging.warning("Ignoring options {} for instance {}".format(ls_errors,item))
+                    for option in [x for x in ls if len(x) == 2]:
+                        files[option[0]] = option[1]
 
                 logging.debug("Thread {} sending request".format(thread_num))
                 response = requests.post(url + "/process", files=files, headers={'host': hostname} if hostname else {})
@@ -295,11 +308,10 @@ def worker(thread_num,database_file,timeout,url,hostname):
             # Handle empty queue here
             break
         except requests.exceptions.RequestException as e:
-            logging.critical("Error {}. Connection request exception {}".format(unicode(e)))
+            logging.critical("Error {}. Connection request exception {}".format(item,unicode(e)))
             QUEUE.task_done()
         except requests.exceptions.ConnectionError as e:
-            logging.error("Error {}. Connection error {}".format(
-                item, e))
+            logging.error("Error {}. Connection error {}".format(item, e))
             QUEUE.task_done()
 
     connection.close()
@@ -377,7 +389,7 @@ def send_jobs(server_url,
         for line in row_lines:
             if line.strip():
                 lines.append(line.replace("\n","").split('|'))
-                if len(lines[-1]) != 3:
+                if len(lines[-1]) != 4:
                     logging.critical("The line {} of the request file {} is not formatted correctly".format(
                         line.strip(),request_file))
                     sys.exit(1)
@@ -499,7 +511,74 @@ def generate_kb_files(
     logging.info("To generate the KB directory test_kb locally run 'csv2kb.py -p ./ test_kb {} {}'".format(feature_file,info_file))
 cli.add_command(generate_kb_files)
 
+################################
+# Check the KB for anomalies and solver errors
+################################
+@click.command()
+@click.option('--database-file',
+              help='Sqlite database file',
+              type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=True, readable=True,
+                              resolve_path=True),
+              default='sunny_db.db')
+def check_anomalies(
+        database_file,
+        ):
+    """
+    Check for possible anomalies in the results
+    """
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
 
+    # get all the results
+    results = {}
+    for row in cursor.execute("SELECT id,solvers,output FROM results", ()):
+        if row[0] not in results:
+            results[row[0]] = {}
+        results[row[0]][row[1]] = json.loads(row[2])
+
+    instances = {}
+    for row in cursor.execute("SELECT id,mzn,dzn,type FROM instances", ()):
+        instances[row[0]] = (row[1:])
+
+    # list all real unk (the ones where the solver terminated before the timeout) or unb
+    for i in results:
+        for j in results[i]:
+            if results[i][j]["result"] == "unb":
+                logging.info("UNBOUND: instance {}, solver {}".format(instances[i],j))
+            elif results[i][j]["result"] == "unk" and results[i][j]["time"] > 0:
+                logging.info("UNKNOWN: instance {}, solver {}".format(instances[i][0:2], j))
+
+    # opt value is consistent
+    for i in results:
+        if instances[i][2] == "sat":
+            sat = [j for j in results[i] if results[i][j]["result"] == "sat"]
+            unsat = [j for j in results[i] if results[i][j]["result"] == "uns"]
+            if sat and unsat:
+                logging.info("SAT-UNSAT inconsistency: instance {}, sat {}, unsat {}".format(instances[i][0:2],sat,unsat))
+        else: # min or max problems
+            best_values = {}
+            for j in results[i]:
+                if results[i][j]["solutions"]:
+                    if instances[i][2] == "min":
+                        best_values[j] = min(results[i][j]["solutions"].values())
+                    else:
+                        best_values[j] = max(results[i][j]["solutions"].values())
+            op_values = set()
+            op_values.update([best_values[j] for j in best_values if results[i][j]["result"] == "opt"])
+            if len(op_values) > 1:
+                logging.info("TWO OPTIMA: instance {}, values {}".format(
+                    instances[i][0:2],{x: best_values[x] for x in best_values if results[i][x]["result"] == "opt"}))
+            if op_values:
+                if instances[i][2] == "min" and min(op_values) > min(best_values.values()):
+                    logging.info("MIN less than OPTIMA: instance {}, optima {}, values {}".format(
+                        instances[i][0:2], min(op_values),
+                        {x: best_values[x] for x in best_values if best_values[x] < min(op_values)}))
+                elif instances[i][2] == "max" and max(op_values) > max(best_values.values()):
+                    logging.info("MAX greater than OPTIMA: instance {}, optima {}, values {}".format(
+                        instances[i][0:2], min(op_values),
+                        {x: best_values[x] for x in best_values if best_values[x] > max(op_values)}))
+    pass
+cli.add_command(check_anomalies)
 
 if __name__ == '__main__':
     cli()
