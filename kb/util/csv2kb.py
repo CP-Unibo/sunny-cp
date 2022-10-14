9eb0f55c1da8e559490726c74805d62b9ae187bf
robama <amadini@cs.unibo.it>
2022-10-14 14:43:32 +0200

    pycodestyled csv2kb

--- kb/util/csv2kb.py
+++ kb/util/csv2kb.py
@@ -21,12 +21,12 @@ Options:
     By default, lb = -1 and ub = 1.
 
   -s <lb>,<ub>
-    Computes the solving score of the COPs in <INFO_FILE> by scaling the partial
+    Computes the solving score of the COPs in <INFO_FILE> by scaling partial
     solutions in the range [lb, ub], where 0 <= lb < lb <= 1.
     By default, lb = 0.25 and ub = 0.75
 
   -a <lb>,<ub>
-    Computes the solving area of the COPs in <INFO_FILE> by scaling the partial
+    Computes the solving area of the COPs in <INFO_FILE> by scaling partial
     solutions in the range [lb, ub], where 0 <= lb < lb <= 1.
     By default, lb = 0.25 and ub = 0.75
 
@@ -44,111 +44,120 @@ import getopt
 import traceback
 from helper_kb import *
 
+
 def main(args):
 
-  # Getting arguments.
-  try:
-    opts, args = getopt.getopt(
-      args, 'ht:p:s:a:f:', ['help', 'no-scale', 'no-const', 'no-check']
-    )
-  except getopt.error as msg:
-    print(msg)
-    print('For help use --help', file=sys.stderr)
-    sys.exit(2)
+    # Getting arguments.
+    try:
+        opts, args = getopt.getopt(
+            args, 'ht:p:s:a:f:', ['help', 'no-scale', 'no-const', 'no-check']
+        )
+    except getopt.error as msg:
+        print(msg)
+        print('For help use --help', file=sys.stderr)
+        sys.exit(2)
 
-  if len(args) != 3:
+    if len(args) != 3:
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                print(__doc__)
+                sys.exit(0)
+        print('Error! Wrong number of arguments.', file=sys.stderr)
+        print('For help use --help', file=sys.stderr)
+        sys.exit(2)
+
+    kb_name = args[0]
+    feat_file = args[1]
+    info_file = args[2]
+    timeout = 1800
+    SUNNY_HOME = os.path.realpath(__file__).split('/')[:-2]
+    path = '/'.join(SUNNY_HOME) + '/kb'
+    lb_feat = -1
+    ub_feat = 1
+    lb_score = 0.25
+    ub_score = 0.75
+    lb_area = 0.25
+    ub_area = 0.75
+    scale = True
+    const = True
+    check = True
+
+    # Arguments parsing.
     for o, a in opts:
-      if o in ('-h', '--help'):
-        print(__doc__)
-        sys.exit(0)
-    print('Error! Wrong number of arguments.', file=sys.stderr)
-    print('For help use --help', file=sys.stderr)
-    sys.exit(2)
+        if o in ('-h', '--help'):
+            print(__doc__)
+            return
+        elif o == '-t':
+            timeout = int(a)
+            if timeout <= 0:
+                print('Error! Timeout', timeout, 'is non positive.',
+                      file=sys.stderr)
+                sys.exit(2)
+        elif o == '-p':
+            path = a
+            if not os.path.exists(a):
+                print('Error! Path', path, 'does not exists.',
+                      file=sys.stderr)
+                sys.exit(2)
+        elif o == '-f':
+            l, u = a.split(',')
+            lb_feat = float(l)
+            ub_feat = float(u)
+            if lb_feat >= ub_feat:
+                print('Error! Bound', l, 'not greater than', u,
+                      file=sys.stderr)
+                sys.exit(2)
+        elif o == '-s':
+            l, u = a.split(',')
+            lb_score = float(l)
+            ub_score = float(u)
+            if not (0 <= lb_score < ub_score <= 1):
+                print('Error! The score must be 0 <=', l, '<', u, '<= 1',
+                      file=sys.stderr)
+                sys.exit(2)
+        elif o == '-a':
+            l, u = a.split(',')
+            lb_area = float(l)
+            ub_area = float(u)
+            if not (0 <= lb_area < ub_area <= 1):
+                print('Error! Must be 0 <=', l, '<', u, '<= 1',
+                      file=sys.stderr)
+                sys.exit(2)
+        elif o == '--no-scale':
+            scale = False
+        elif o == '--no-const':
+            const = False
+        elif o == '--no-check':
+            check = False
 
-  kb_name = args[0]
-  feat_file = args[1]
-  info_file = args[2]
-  timeout = 1800
-  SUNNY_HOME = os.path.realpath(__file__).split('/')[:-2]
-  path = '/'.join(SUNNY_HOME) + '/kb'
-  lb_feat = -1
-  ub_feat = 1
-  lb_score = 0.25
-  ub_score = 0.75
-  lb_area = 0.25
-  ub_area = 0.75
-  scale = True
-  const = True
-  check = True
-
-  # Arguments parsing.
-  for o, a in opts:
-    if o in ('-h', '--help'):
-      print(__doc__)
-      return
-    elif o == '-t':
-      timeout = int(a)
-      if timeout <= 0:
-        print('Error! Timeout',timeout,'is non positive.', file=sys.stderr)
-        sys.exit(2)
-    elif o == '-p':
-      path = a
-      if not os.path.exists(a):
-        print('Error! Path',path,'does not exists.', file=sys.stderr)
-        sys.exit(2)
-    elif o == '-f':
-      l, u = a.split(',')
-      lb_feat = float(l)
-      ub_feat = float(u)
-      if lb_feat >= ub_feat:
-        print('Error! Bound',l,'not greater than',u, file=sys.stderr)
-        sys.exit(2)
-    elif o == '-s':
-      l, u = a.split(',')
-      lb_score = float(l)
-      ub_score = float(u)
-      if not (0 <= lb_score < ub_score <= 1):
-        print('Error! Must be 0 <=',l,'<',u,'<= 1', file=sys.stderr)
-        sys.exit(2)
-    elif o == '-a':
-      l, u = a.split(',')
-      lb_area = float(l)
-      ub_area = float(u)
-      if not (0 <= lb_area < ub_area <= 1):
-        print('Error! Must be 0 <=',l,'<',u,'<= 1', file=sys.stderr)
-        sys.exit(2)
-    elif o == '--no-scale':
-      scale = False
-    elif o == '--no-const':
-      const = False
-    elif o == '--no-check':
-      check = False
-
-  kb_path = path + '/' + kb_name
-  if os.path.exists(kb_path):
-    print('Error! Folder',kb_path,'already exists! Choose ', end=' ', file=sys.stderr)
-    print('another name or location for the knowledge base!', file=sys.stderr)
-    sys.exit(2)
-
-  try:
-    # Extract runtimes information.
-    kb_csp, kb_cop = compute_infos(
-      info_file, timeout, lb_score, ub_score, lb_area, ub_area, check
-    )
-    # Process the features and make the knowledge base.
-    make_kb(
-      kb_path, kb_name, feat_file, lb_feat, ub_feat,
-      scale, const, kb_csp, kb_cop
-    )
-  except Exception as e:
-    traceback.print_exc()
+    kb_path = path + '/' + kb_name
     if os.path.exists(kb_path):
-      from shutil import rmtree
-      rmtree(kb_path)
-    print('Knowledge base',kb_name,'not created')
-    sys.exit(1)
+        print('Error! Folder', kb_path, 'already exists! Choose ', end=' ',
+              file=sys.stderr)
+        print('another name or location for the knowledge base!',
+              file=sys.stderr)
+        sys.exit(2)
 
-  print('Knowledge base',kb_name,'created in',path)
+    try:
+        # Extract runtimes information.
+        kb_csp, kb_cop = compute_infos(
+            info_file, timeout, lb_score, ub_score, lb_area, ub_area, check
+        )
+        # Process the features and make the knowledge base.
+        make_kb(
+            kb_path, kb_name, feat_file, lb_feat, ub_feat,
+            scale, const, kb_csp, kb_cop
+        )
+    except Exception as e:
+        traceback.print_exc()
+        if os.path.exists(kb_path):
+            from shutil import rmtree
+            rmtree(kb_path)
+        print('Knowledge base', kb_name, 'not created')
+        sys.exit(1)
+
+    print('Knowledge base', kb_name, 'created in', path)
+
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+    main(sys.argv[1:])
